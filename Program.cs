63f308d289ee09fa52642284e96bc2e43d718ffe
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-07-29 14:17:52 -0600

    integers and doubles in csharp

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿string greeting = "Hello";
-greeting = greeting + "World";
-Console.WriteLine("greeting" + greeting);
+﻿Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
+Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
+Console.WriteLine(1 + 2 * 3);     // order of operations
+Console.WriteLine(10 / 3.0);      // int's and doubles
+Console.WriteLine(10 / 3);        // int's 
+Console.WriteLine("12" + "3");    // What happens here?
+int num = 10;
+num += 100;
+Console.WriteLine(num);
+num ++;
+Console.WriteLine(num);
