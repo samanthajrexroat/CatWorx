b0d2369aca85d5d94a988fbcd7cb87feeeb164d5
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-07-29 14:44:52 -0600

    populating dictionaries

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
-﻿string stringNum = "2";
-int intNum = Convert.ToInt16(stringNum);
-Console.WriteLine(intNum);
-Console.WriteLine(intNum.GetType());
+﻿Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
+    { "firstInning", 10 },
+    { "secondInning", 20},
+    { "thirdInning", 30},
+    { "fourthInning", 40},
+    { "fifthInning", 50}
+};
+
+Console.WriteLine("----------------");
+Console.WriteLine("  Scoreboard");
+Console.WriteLine("----------------");
+Console.WriteLine("Inning |  Score");
+Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
+Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
+Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
+Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
+Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
