411b1edbe207e5c5a3ea982efa596c1f381738f6
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-07-29 14:51:35 -0600

    array declaration

--- Program.cs
+++ Program.cs
@@ -1,17 +1,5 @@
-﻿Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
-    { "firstInning", 10 },
-    { "secondInning", 20},
-    { "thirdInning", 30},
-    { "fourthInning", 40},
-    { "fifthInning", 50}
-};
-
-Console.WriteLine("----------------");
-Console.WriteLine("  Scoreboard");
-Console.WriteLine("----------------");
-Console.WriteLine("Inning |  Score");
-Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
-Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
-Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
-Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
-Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+﻿string[] favFoods = new string[3]{ "pizza", "doughnuts", "icecream" };
+// string firstFood = favFoods[0];
+// string secondFood = favFoods[1];
+// string thirdFood = favFoods[2];
+Console.WriteLine("I like {0}, {1}, and {2}", favFoods[0], favFoods[1], favFoods[2]);
