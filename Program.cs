e0914e378b8a4ef8ab27078b0c479f6e37bf9cbc
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-07-29 14:32:53 -0600

    DataType conversion in csharp

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-﻿Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, *
-Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3
-Console.WriteLine(1 + 2 * 3);     // order of operations
-Console.WriteLine(10 / 3.0);      // int's and doubles
-Console.WriteLine(10 / 3);        // int's 
-Console.WriteLine("12" + "3");    // What happens here?
-int num = 10;
-num += 100;
-Console.WriteLine(num);
-num ++;
-Console.WriteLine(num);
+﻿string stringNum = "2";
+int intNum = Convert.ToInt16(stringNum);
+Console.WriteLine(intNum);
+Console.WriteLine(intNum.GetType());
